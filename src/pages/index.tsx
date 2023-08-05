beb3af169a532d9711e4924736d12a2f64ee5747
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-08-05 18:16:36 +0100

    main logic complete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,64 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Head from 'next/head' 
-import { useState } from 'react'
-import { DONATION_IN_NAIRA, MAX_DONATION_IN_NAIRA } from '../../config'
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
+// import { Inter } from '@next/font/google'
+import Head from "next/head";
+import { useState } from "react";
+import { DONATION_IN_KOBO, MAX_DONATION_IN_KOBO } from "../../config";
+// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-
-  const [quantity, setQuantity] = useState(1)
-  const [error, setError] = useState(null)
-  const [name, setName] = useState("")
-  const [message, setMessage] = useState("")
-  const presets =[100,200,500]
+  const [quantity, setQuantity] = useState(1);
+  const [error, setError] = useState(null);
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const presets = [1, 2, 5];
   return (
-  <><Head> 
-
-    </Head><div>
-      <main>
-        <div>
-          <h1>
-       {   presets.map((preset)=>{
-            return(
-              <button key={preset} onClick={()=>setQuantity(preset)}>{preset}</button>
-            )
-          })}
-          
-          <input 
-          type="number" 
-          onChange={(e) => setQuantity(parseFloat(e.target.value))}
-          value={quantity}
-          min={1}
-          max={MAX_DONATION_IN_NAIRA/DONATION_IN_NAIRA}
-          /></h1>
+    <>
+      <Head>
+        <title>Donations</title>
+      </Head>
+      <div>
+        <main>
           <div>
+            <h1>
+              Send a donation
+              {presets.map((preset) => {
+                return (
+                  <button key={preset} onClick={() => setQuantity(preset)}>
+                    {preset}
+                  </button>
+                );
+              })}
+              <input
+                type="number"
+                onChange={(e) => setQuantity(parseFloat(e.target.value))}
+                value={quantity}
+                min={1}
+                max={MAX_DONATION_IN_KOBO / DONATION_IN_KOBO}
+              />
+            </h1>
             <div>
-              <label htmlFor='name'>Name</label>
-              <input type="text" id="name" onChange={(e) => setName(e.target.value)} value={name} />
-            </div>
-             <div>
-              <label htmlFor='message'>Message</label>
-              <textarea id="message" onChange={(e) => setMessage(e.target.value)} value={name} />
+              <div>
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  id="name"
+                  onChange={(e) => setName(e.target.value)}
+                  value={name}
+                />
+              </div>
+              <div>
+                <label htmlFor="message">Message</label>
+                <textarea
+                  id="message"
+                  onChange={(e) => setMessage(e.target.value)}
+                  value={message}
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <button>Donate #{quantity*(DONATION_IN_NAIRA) }</button>
+          <button>Donate #{quantity * (DONATION_IN_KOBO / 100)}</button>
         </main>
-      </div></>
-  )
+      </div>
+    </>
+  );
 }
