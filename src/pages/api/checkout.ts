0e33c6871dc4974625e73b0bd2a202735a3ab7e1
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-10-02 20:48:06 +0100

    testing paystack

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import Paystack from "paystack";
+const paystack = Paystack("SECRET_KEY");
+
 
 type Data = {
   message: string
@@ -10,10 +13,27 @@ export default function handler(
   res: NextApiResponse<Data>
 ) {
   if (req.method !== "POST"){
-      res.status(400).json({ message: 'Method not allowed' })
+      res.status(405).json({ message: 'Method not allowed' })
   }
 const name = req.body.name || "Anonymous"
 const message = req.body.message || ""
-const qauntity = req.body .quantity || 1 
+const quantity = req.body .quantity || 1 
 //write error handling for if quantity doesnt exist
+
+
+try{
+const session = paystack.transaction.initialize({
+    name: "Transaction or product name",
+    email: "The user email",
+    amount: amount * 100, // this because paystack also send kobo
+    quantity: "quantity of product just for reference purposes",
+    callback_url: "redirect URL"
+    metadata:{
+         //other information you want to send with the transaction and check when verifying
+         userId: ""
+     }
+  }).then(async(transaction) =>{
+  console.log(transaction)
+}}catch(e){}
+res.status(200).json()
 }
