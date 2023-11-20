c2fd9b7fab796d3443334f1cd5877facd524a59b
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-11-20 17:30:55 +0100

    ignore

--- src/components/donations.tsx
+++ src/components/donations.tsx
@@ -1,17 +1,18 @@
-const Donations = () => {
-  return (
-    <div>
-      {" "}
-      {donations.map(() => {
-        return (
-          <div key={donation.id} className="p-4 shadow mb-2">
-            {donations.fields.name} donated ${donation.fields.amount} <br />{" "}
-            {donation.fields.message}
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+// const Donations = () => {
+//   return (
+//     <div>
+//       {" "}
+//       {donations.map(() => {
+//         return (
+//           <div key={donation.id} className="p-4 shadow mb-2">
+//             {donations.fields.name} donated ${donation.fields.amount} <br />{" "}
+//             {donation.fields.message}
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// };
 
-export default Donations;
+// export default Donations;
+// //
