33a9748f4fc358c846a4b609a57f94001ab15120
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-07-26 15:14:14 +0100

    refactor: Update UI components for Avatar and Checkbox

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,13 @@ import DonationForm from "@/components/donationForm";
 import Donations from "@/components/recentDonations";
 export default function Home() {
   return (
-    <main>
-      <div className="flex flex-col md:flex-row mx-auto pt-10 justify-between">
-        <MainCard />
+    <>
+      <main>
         <DonationForm />
-      </div>
-      <div className="mx-auto p-10">
-        <Donations />
-      </div>
-    </main>
+        <div className="mx-auto p-10">
+          <Donations />
+        </div>
+      </main>
+    </>
   );
 }
