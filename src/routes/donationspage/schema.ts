c151794beb0bffe14a797f3b718542244d7337c5
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2025-03-31 16:01:28 +0100

    fix: make donation amount optional in the donations schema

--- src/routes/donationspage/schema.ts
+++ src/routes/donationspage/schema.ts
@@ -4,7 +4,7 @@ export const donationSchema = z.object({
 	name: z.string().min(1, { message: 'Name is required.' }),
 	donationEmail: z.string().email({ message: 'Invalid email address.' }),
 	message: z.string().optional(),
-	amount: z.number().min(100, { message: 'Minimum donation is 100 Naira.' }),
+	amount: z.number().min(100, { message: 'Minimum donation is 100 Naira.' }).optional(),
 	selectedPreset: z.number().optional(),
 	selectedDonation: z.string().optional()
 	// privateMessage: z.boolean().optional()
