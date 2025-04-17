53c4b8e21493ae63ac006fb2578a8f4ee4282257
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2025-04-17 19:37:27 +0100

    chore: fancy stuff

--- src/routes/donationspage/schema.ts
+++ src/routes/donationspage/schema.ts
@@ -1,13 +1,16 @@
 import { z } from 'zod';
 
 export const donationSchema = z.object({
-	name: z.string().min(1, { message: 'Name is required.' }),
-	donationEmail: z.string().email({ message: 'Invalid email address.' }),
+	name: z.string().min(1, { message: 'Name is required' }),
+	donationEmail: z.string().email({ message: 'Valid email is required' }),
 	message: z.string().optional(),
-	amount: z.number().min(100, { message: 'Minimum donation is 100 Naira.' }).optional(),
+	amount: z
+		.number()
+		.min(100, { message: 'Minimum donation is ₦100' })
+		.max(10000, { message: 'Maximum donation is ₦10,000' })
+		.optional(),
 	selectedPreset: z.number().optional(),
-	selectedDonation: z.string().optional()
-	// privateMessage: z.boolean().optional()
+	selectedDonation: z.enum(['one-time', 'monthly']).default('one-time').optional()
 });
 
 export type FormSchema = typeof donationSchema;
