0f4182c2f79e8f2867d0e60dee6f46d820ab8596
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2025-04-28 17:49:00 +0100

    chore: :art: update types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,43 @@
-// See https://svelte.dev/docs/kit/types#app.d.ts
-// for information about these interfaces
 declare global {
 	namespace App {
+		interface ValidationMessage<T = Record<string, string>> {
+			status: 'error' | 'success' | 'warning';
+			text: string;
+			data?: T;
+		}
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
+	}
+
+	interface Window {
+		PaystackPop: {
+			new (): PaystackPopInstance;
+		};
+	}
+
+	interface PaystackPopInstance {
+		newTransaction(config: PaystackConfig): void;
+	}
+
+	interface PaystackConfig {
+		key: string;
+		email: string;
+		amount: number;
+		currency?: string;
+		metadata?: Record<string, unknown>;
+		onSuccess?: (response: {
+			reference: string;
+			trans: string;
+			status: 'success' | 'failed' | 'pending';
+			message: string;
+			transaction: string;
+			trxref: string;
+			redirecturl: string;
+		}) => void;
+		onClose?: () => void;
 	}
 }
 
