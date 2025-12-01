c0bf86eabf042ed5d94f3e0267bd4f36a260cc71
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2025-12-01 22:34:21 +0100

    OG plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { sveltekitOG } from '@ethercorps/sveltekit-og/plugin';
+
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()]
+	plugins: [sveltekit(), tailwindcss(), sveltekitOG()]
 });
